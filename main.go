304fbbfdd0774ca6bb840568db1522d197901f5e
Dan Finn <dan.finn@plansource.com>
2020-09-11 16:23:12 -0600

    added a check to make sure target directory exists

--- main.go
+++ main.go
@@ -14,6 +14,11 @@ func main() {
 	dir := flag.String("d", ".", "Directory to serve")
 	flag.Parse()
 
+	if _, err := os.Stat(*dir); os.IsNotExist(err) {
+		// Check if target directory exists
+		log.Fatal("Directory does not exist")
+	}
+
 	http.Handle("/", http.FileServer(http.Dir(*dir)))
 	log.Fatal(http.ListenAndServe(":"+*port, apachelog.CombinedLog.Wrap(http.FileServer(http.Dir(*dir)), os.Stderr)))
 
